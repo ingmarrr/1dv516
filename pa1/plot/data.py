54f3c9d8232625511b9ea4469cab67d59c58b05c
Ingmar Falk <if222gq@student.lnu.se>
2023-09-08 16:24:13 +0200

    Removed unnecessary code

--- pa1/plot/data.py
+++ pa1/plot/data.py
@@ -19,13 +19,3 @@ if __name__ == "__main__":
     print("This is the data module. Dont run it directly.")
     pass
 
-    match path:
-        case "qf":
-            qfdata = d.load_csv("qfres.csv")
-            v.plot(qfdata, "QUnionFind", "Size", "Time in Ms", "qfres")
-            v.plot_scale_y(qfdata, "QUnionFind", "Size", "Time in Ms", "qfres", 100)
-        case "wqf":
-            wqfdata = d.load_csv("wqfres.csv")
-            v.plot(wqfdata, "WQUnionFind", "Size", "Time in Ms", "wqfres")
-        case _:
-            print("Invalid argument passed to script")
